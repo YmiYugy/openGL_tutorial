c2af6c29c34e470ad4aaa5b0c887f976340e73aa
Tristan Kobusch <tristan.kobusch@gmail.com>
2018-04-23 20:30:11 +0200

    refactoring
    now everything is organized in a class and proper exception handling is implemented.

--- source/main.cpp
+++ source/main.cpp
@@ -1,57 +1,94 @@
+//
+// Created by trist on 23.04.2018.
+//
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int heigt);
-
-void processInput(GLFWwindow *window);
+#include <functional>
 
 
-int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+class TriangleApplication {
+public:
+    void run() {
+        init();
+        mainLoop();
+        cleanup();
+    }
 
-    GLFWwindow *window = glfwCreateWindow(700, 600, "LearnOpenGL", nullptr, nullptr);
-    if (window == NULL) {
-        std::cerr << "Failed to create GLFW window\n";
+private:
+    GLFWwindow *window;
+
+    void init() {
+        initWindow();
+        initGL();
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            processInput();
+            draw();
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
         glfwTerminate();
-        return -1;
     }
-    glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
-        return -1;
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        window = glfwCreateWindow(700, 600, "LearnOpenGL", nullptr, nullptr);
+
+        if (window == NULL) {
+            glfwTerminate();
+            throw std::runtime_error("failed to create GLFW window\n");
+        }
     }
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    while (!glfwWindowShouldClose(window)) {
-        // input
-        processInput(window);
+    void initGL() {
+        glfwMakeContextCurrent(window);
 
-        // rendering
+        if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+            throw std::runtime_error("failed to initialize GLAD\n");
+        }
+
+        glViewport(0, 0, 800, 600);
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    }
+
+    static void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    void processInput() {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, true);
+        }
+    }
+
+    void draw() {
+        // clear window
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // check events and swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
     }
 
 
-    glfwTerminate();
-    return 0;
-}
+};
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int heigt) {
-    glViewport(0, 0, width, heigt);
-}
-
-void processInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
+int main() {
+    TriangleApplication app;
+    try {
+        app.run();
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << "\n";
+        return EXIT_FAILURE;
     }
+
+    return EXIT_SUCCESS;
 }
