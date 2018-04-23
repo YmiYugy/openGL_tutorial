91b0241ab50335e446daa0a9b07a9c60f819b82e
Tristan Kobusch <tristan.kobusch@gmail.com>
2018-04-23 19:43:51 +0200

    window creation

--- source/main.cpp
+++ source/main.cpp
@@ -1,66 +1,41 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
-#include <stdio.h>
+void framebuffer_size_callback(GLFWwindow *window, int width, int heigt);
 
-namespace {
-    void errorCallback(int error, const char* description) {
-        fprintf(stderr, "GLFW error %d: %s\n", error, description);
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(700, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
     }
+    glfwMakeContextCurrent(window);
 
-    GLFWwindow* initialize() {
-        int glfwInitRes = glfwInit();
-        if (!glfwInitRes) {
-            fprintf(stderr, "Unable to initialize GLFW\n");
-            return nullptr;
-        }
-
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-        GLFWwindow* window = glfwCreateWindow(1280, 720, "InitGL", nullptr, nullptr);
-        if (!window) {
-            fprintf(stderr, "Unable to create GLFW window\n");
-            glfwTerminate();
-            return nullptr;
-        }
-
-        glfwMakeContextCurrent(window);
-
-        int gladInitRes = gladLoadGL();
-        if (!gladInitRes) {
-            fprintf(stderr, "Unable to initialize glad\n");
-            glfwDestroyWindow(window);
-            glfwTerminate();
-            return nullptr;
-        }
-
-        return window;
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
     }
-}
-
-int main(int argc, char* argv[]) {
-    glfwSetErrorCallback(errorCallback);
-
-    GLFWwindow* window = initialize();
-    if (!window) {
-        return 0;
-    }
-
-    // Set the clear color to a nice green
-    glClearColor(0.15f, 0.6f, 0.4f, 1.0f);
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
 
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int heigt) {
+    glViewport(0, 0, width, heigt);
 }
