d9b778051dda9d87c4ed04fee3cae7a0609552cd
Tristan Kobusch <tristan.kobusch@gmail.com>
2018-04-24 16:16:24 +0200

    add glad to git get openGl working and draw a rectangle get window resizing work properly add shaders

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <functional>
+#include <fstream>
+#include <thread>
 
 
 class TriangleApplication {
@@ -18,22 +20,43 @@ public:
 
 private:
     GLFWwindow *window;
+    std::vector<float> vertices = {
+            0.5f, 0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f, 0.5f, 0.0f   // top left
+    };
+    std::vector<unsigned int> indices = {
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+    };
+    unsigned int VBO;
+    unsigned int shaderProgram;
+    unsigned int VAO;
+    unsigned int EBO;
 
     void init() {
         initWindow();
         initGL();
+        initGraphicsPipeline();
     }
 
     void mainLoop() {
         while (!glfwWindowShouldClose(window)) {
             processInput();
             draw();
-            glfwSwapBuffers(window);
+
             glfwPollEvents();
+            glfwSwapBuffers(window);
         }
     }
 
     void cleanup() {
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
+
+
         glfwTerminate();
     }
 
@@ -49,6 +72,7 @@ private:
             glfwTerminate();
             throw std::runtime_error("failed to create GLFW window\n");
         }
+        glfwSetWindowUserPointer(window, this);
     }
 
     void initGL() {
@@ -58,13 +82,106 @@ private:
             throw std::runtime_error("failed to initialize GLAD\n");
         }
 
-        glViewport(0, 0, 800, 600);
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+        glfwSetWindowRefreshCallback(window, window_refresh_callback);
+
     }
+
+    void initGraphicsPipeline() {
+        // load, compile, link, and use shaders
+        initShader();
+
+        // init VBO and VAO and EBO
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+        glEnableVertexAttribArray(0);
+
+        glBindVertexArray(0);
+
+        // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+    }
+
+
+    void initShader() {
+        auto loadShader = [](const std::string &path, unsigned int &shader, GLenum type) -> void {
+            std::ifstream file(path);
+            if (!file.is_open()) {
+                std::runtime_error("failed to open " + path);
+            }
+            std::string contents{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+            const char *content = contents.c_str();
+
+            shader = glCreateShader(type);
+            glShaderSource(shader, 1, &content, nullptr);
+            glCompileShader(shader);
+
+            int success;
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                int length;
+                glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+                std::vector<char> log(length);
+                glGetShaderInfoLog(shader, length, &length, log.data());
+
+                throw std::runtime_error("failed to compile shader\n" + std::string(log.data()));
+            }
+        };
+        unsigned int vertexShader;
+        unsigned int fragmentShader;
+
+        loadShader("../shaders/shader.vert", vertexShader, GL_VERTEX_SHADER);
+        loadShader("../shaders/shader.frag", fragmentShader, GL_FRAGMENT_SHADER);
+
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        int success;
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success) {
+            int length;
+            glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &length);
+            std::vector<char> log(length);
+            glGetProgramInfoLog(shaderProgram, length, nullptr, log.data());
+            throw std::runtime_error("failed to compile shader program" + std::string(log.data()));
+        }
+
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+    }
+
+
 
     static void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
         glViewport(0, 0, width, height);
     }
+
+    static void window_refresh_callback(GLFWwindow *window) {
+        TriangleApplication *app = reinterpret_cast<TriangleApplication *>(glfwGetWindowUserPointer(window));
+        if (!app) {
+            std::cout << "null";
+        } else {
+            app->draw();
+        }
+        glfwSwapBuffers(window);
+    }
+
 
     void processInput() {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -76,6 +193,14 @@ private:
         // clear window
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+
+
     }
 
 
@@ -89,6 +214,7 @@ int main() {
         std::cerr << e.what() << "\n";
         return EXIT_FAILURE;
     }
+
 
     return EXIT_SUCCESS;
 }
